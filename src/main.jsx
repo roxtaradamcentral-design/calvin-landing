b263e2cdc902aa6dfd6b035ed31929cd1dad5bb9
roxtaradamcentral-design <roxtaradamcentral@gmail.com>
2026-07-28 17:20:39 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CoachingLandingPage from "./CoachingLandingPage.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CoachingLandingPage />
+  </React.StrictMode>
+);
